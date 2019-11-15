8ba196d74a01aeacb88ff2cf4815132366060003
SeniorZhai <developer.zhaitao@gmail.com>
2019-11-15 15:15:01 +0800

    List activity

---
+++ app/src/main/java/io/seniorzhai/motion/demo/BasicActivity.kt
@@ -0,0 +1,12 @@
+package io.seniorzhai.motion.demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class BasicActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basic)
+    }
+}
--- app/src/main/java/io/seniorzhai/motion/demo/MainActivity.kt
+++ app/src/main/java/io/seniorzhai/motion/demo/MainActivity.kt
@@ -1,12 +1,42 @@
 package io.seniorzhai.motion.demo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.item_title.view.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        list.adapter = ItemAdapter(listOf("aaa")) { position ->
+            if (position == 0) {
+                startActivity(Intent(this, BasicActivity::class.java))
+            }
+        }
     }
+
+    class ItemAdapter(private val titles: List<String>, val action: (Int) -> Unit) :
+        RecyclerView.Adapter<ItemHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+            val context = parent.context
+            val itemView = LayoutInflater.from(context).inflate(R.layout.item_title, parent, false)
+            return ItemHolder(itemView)
+        }
+
+        override fun getItemCount(): Int = titles.size
+
+        override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+            holder.itemView.title.text = titles[position]
+            holder.itemView.setOnClickListener { action(position) }
+        }
+    }
+
+    class ItemHolder(view: View) : RecyclerView.ViewHolder(view)
 }
